1b58463332a0d3055bf3c8902994ac2bc3f876a2
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2023-10-05 13:31:52 +0200

    aggiornato al punto 8

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,6 +3,9 @@ from prima_app.views import homepage
 from prima_app.views import welcome
 from prima_app.views import lista
 from prima_app.views import chiSiamo
+from prima_app.views import variabili
+from prima_app.views import index
+
 
 
 
@@ -12,5 +15,6 @@ urlpatterns=[
     path('welcome', welcome, name='welcome'),
     path('lista', lista, name='lista'),
     path('chiSiamo', chiSiamo, name='chiSiamo'),
-
+    path('variabili', variabili, name='variabili'),
+    path('index', index, name='index'),
 ]

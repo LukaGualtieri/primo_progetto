d6133b2c674cc4b4d1041c04f1cd1d51e03f7f42
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2023-10-20 09:30:53 +0200

    Punto 11 e 12

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -4,5 +4,9 @@ from seconda_app.views import *
 
 app_name="seconda_app"
 urlpatterns=[
+    path('', index, name='index'),
     path('es_if', es_if, name='es_if'),
+    path('if_else_elif', if_else_elif, name='if_else_elif'),
+    path('es_for', es_for, name='es_for'),
+
 ]

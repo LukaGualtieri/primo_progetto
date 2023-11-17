5e942f378000a0487ef4408d7f00d3b82179048d
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2023-11-17 09:59:27 +0100

    Passo 19 e Passo 20 punto 2

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ class Giornalista(models.Model):
     """il modello generico di un giornalista"""
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
-    #anno_di_nascita = models.DateField(blank=True)
+    anno_di_nascita = models.DateField(blank=True)
 
     def __str__(self):
         return self.nome + " " + self.cognome
@@ -20,8 +20,8 @@ class Articolo(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli") 
-    #data = models.DateField(auto_now=True, blank=True)
-    #visualizzazioni = models.IntegerField(default=0, blank=True) 
+    data = models.DateField(auto_now=False, blank=True)
+    visualizzazioni = models.IntegerField(default=0, blank=True) 
 
     def __str__(self):
         return self.titolo
--- news/views.py
+++ news/views.py
@@ -67,13 +67,13 @@ def listaArticoli(request, pk=None):
 
 def queryBase(request):
     #1.Tutti gli articoli scritti da giornalisti di un certo cognome:
-    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Rossi')
+    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Gualtieri')
     
     #2.Totale
     numero_totale_articoli = Articolo.objects.count()
 
     #3.Contare il numero di articoli scritti da un giornalista specifico:
-    giornalista_1 = Giornalista.objects.get(id=1)
+    giornalista_1 = Giornalista.objects.get(id=5)
     numero_articoli_giornalista_1 = Articolo.objects.filter(giornalista=giornalista_1).count()
 
     #4.Ordinare gli articoli per numero di visualizzazioni in ordine decrescente
@@ -89,10 +89,10 @@ def queryBase(request):
     giornalisti_data = Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1990, 1, 1))
     
     #8.Tutti gli articoli pubblicati in una data specifica:
-    articoli_del_giorno = Articolo.objects.filter(data=datetime.date(2023, 1, 1))
+    articoli_del_giorno = Articolo.objects.filter(data=datetime.date(2022, 11, 14))
     
     #9.Tutti gli articoli pubblicati in un intervallo di date
-    articoli_periodo  = Articolo.objects.filter(data__range=(datetime.date(2023, 1, 1), datetime.date(2023, 12, 31)))
+    articoli_periodo  = Articolo.objects.filter(data__range=(datetime.date(1990, 1, 1), datetime.date(2023, 12, 31)))
     
     #10.Gli articoli scritti da giornalisti nati prima del 1980:
     giornalisti_nati = Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(1980, 1, 1)) 
@@ -111,9 +111,46 @@ def queryBase(request):
     articoli_minime_visualizzazioni = Articolo.objects.filter(visualizzazioni__gte=100)
     
     #15.Tutti gli articoli che contengono una certa parola nel titolo: 
-    articoli_parola = Articolo.objects.filter(titolo_icontains='importante')
+    articoli_parola = Articolo.objects.filter(titolo__icontains='ferrari')
 
-    # Creare il dizionario context
+    #16.Articoli pubblicati in un certo mese di un anno specifico:
+    #nota per poter modificare la data di un articolo togliere la proprietà auto_now=True al field data nel model 
+    #poi dare i comandi makemigrations e migrate per applicare le modifiche al database
+    articoli_mese_anno = Articolo.objects.filter(data__month=2, data__year=1999)
+
+    #17.Giornalisti con almeno un articolo con più di 100 visualizzazioni
+    giornalisti_con_articoli_popolari = Giornalista.objects.filter(articoli__visualizzazioni__gte=100).distinct()
+    """
+    spiegazione dettagliata:
+    Giornalista.objects: Inizia dalla classe del modello Giornalista.
+    .filter(articoli__visualizzazioni__gte-100): Utilizza il metodo filter() per filtrare i giornalisti 
+    in base al campo visualizzazioni nel modello Articolo. La notazione articoli_visualizzazioni indica 
+    che si sta seguendo la relazione inversa dalla classe Giornalista alla classe Articolo attraverso 
+    il campo ForeignKey giornalista nel modello Articolo.
+    .distinct(): E' un metodo assicura che i risultati siano distinti, eliminando eventuali duplicati.
+    In questo caso, ciò è utile perché un giornalista potrebbe essere associato a più articoli che soddisfano
+    il criterio, e vogliamo ottenere solo una volta ogni giornalista che ha scritto almeno un articolo popolare.
+    """
+
+    #UTILIZZO DI PIU' CONDIZIONI DI SELEZIONE
+    data = datetime.date(2000, 1, 1)
+    visualizzazioni = 150
+    #Per mettere in AND le condizioni separarle con la virgola
+    #18.Tutti gli articoli scritti da dopo il 1/1/2000 e visualizzazioni con valore uguale o maggiore di 150: 
+    articoli_con_and = Articolo.objects.filter(giornalista__anno_di_nascita__gt=data, visualizzazioni__gte=visualizzazioni)
+    
+    #Per mettere in OR le condizioni utilizzare l'operatore Q
+    from django.db.models import Q
+    #19.Tutti gli articoli scritti da dopo il 1/1/2000 o visualizzazioni con valore uguale o maggiore di 150: 
+    articoli_con_or = Articolo.objects.filter(Q(giornalista__anno_di_nascita__gt=data) | Q(visualizzazioni__lte=visualizzazioni))
+    #Per il NOT (~) utilizzare l'operatore Q
+    #20.Tutti gli articoli scritti da prima del 1/1/2000: 
+    articoli_con_not = Articolo.objects.filter(~Q(giornalista__anno_di_nascita__lt=data))
+    #oppure il metodo exclude
+    #con exclude vengono esclusi gli articoli con data minore del 1/1/2000
+    articoli_con_not = Articolo.objects.exclude(giornalista__anno_di_nascita__lt=data)
+
+    #Creare il dizionario context
     context = {
         'articoli_cognome' : articoli_cognome,
         'numero_totale_articoli' : numero_totale_articoli,
@@ -132,5 +169,10 @@ def queryBase(request):
         'ultimi' : ultimi,
         'articoli_minime_visualizzazioni' : articoli_minime_visualizzazioni,
         'articoli_parola' : articoli_parola,
+        'articoli_mese_anno' : articoli_mese_anno,
+        'giornalisti_con_articoli_popolari' : giornalisti_con_articoli_popolari,
+        'articoli_con_and' : articoli_con_and,
+        'articoli_con_or' : articoli_con_or,
+        'articoli_con_not' : articoli_con_not,
     }
     return render(request, 'query.html', context)

93dbc078deab9313ed45513cf408fa349682f7d6
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2023-12-22 10:47:23 +0100

    passo 22 completo

--- voti/views.py
+++ voti/views.py
@@ -45,9 +45,40 @@ def view_c(request):
 
 
 def view_d(request):
+    voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
+            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
+            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
+
+    votoMax=0
+    votoMin=0
+    materieMax=[]
+    materieMin=[]
+    studenteMax=[]
+    studenteMin=[]
+    votii=[]
+
+    for studente, materie in voti.items():
+        for materia,voto,assenze in materie:
+            votii.append(voto)
+    votoMax=max(votii)
+    votoMin=min(votii)
+
+    for studente, materie in voti.items():
+        for materia,voto,assenze in materie:
+            if(voto==votoMax):
+                    materieMax.append(materia)
+                    studenteMax.append(studente)
+            if(voto==votoMin):
+                    materieMin.append(materia)
+                    studenteMin.append(studente)
+
     context={
-        'voti' : {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
-                  'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
-                  'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
+        'voti' : voti,
+        'votoMax' : votoMax,
+        'votoMin' : votoMin,
+        'materieMax' : materieMax,
+        'materieMin' : materieMin,
+        'studenteMax' : studenteMax,
+        'studenteMin' : studenteMin,
     }
     return render(request, "maxMin_voti.html", context)

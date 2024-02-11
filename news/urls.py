f43a3c806090a8b384956df7f691bd876b6deda2
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2024-02-11 12:05:51 +0100

    ripristino

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexNews, home, articoloDetailView, giornalistaDetailView, listaArticoli, queryBase, giornalista_api, giornalisti_list_api, articoli_list_api, articolo_api
+from .views import indexNews, home, articoloDetailView, giornalistaDetailView, listaArticoli, queryBase
 
 app_name = 'news'
 urlpatterns = [
@@ -11,3 +11,18 @@ urlpatterns = [
     path('lista_articoli/', listaArticoli, name="lista_articoli"),
     path('query_base/', queryBase, name="query_base"),
 ]
+
+"""
+urlpatterns = [
+    path('', indexNews, name='indexNews'),
+    path('homeview/', home, name="homeview"),
+    path('articoli/<int:pk>', articoloDetailView, name="articolo_detail"),
+    path('giornalisti/<int:pk>', giornalistaDetailView, name="giornalista_detail"),
+    path('lista_articoli/<int:pk>', listaArticoli, name="lista_articoli_giornalista"),
+    path('lista_articoli/', listaArticoli, name="lista_articoli"),
+    path('query_base/', queryBase, name="query_base"),
+    path('lista_giornalisti_api/', giornalisti_list_api, name="giornalisti_list_api"),
+    path('giornalista_api/<int:pk>', giornalista_api, name="giornalista_api"),
+    path('lista_articoli_api/', articoli_list_api, name="articoli_list_api"),
+    path('articolo_api/<int:pk>', articolo_api, name="articolo_api"),
+]"""
--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 import datetime
 from django.shortcuts import render, get_object_or_404 
 from django.http import HttpResponse
-from django.http import JsonResponse
+#from django.http import JsonResponse
 from .models import Articolo, Giornalista
 
 # Create your views here.
@@ -190,3 +190,57 @@ def queryBase(request):
         'articoli_con_not' : articoli_con_not,
     }
     return render(request, 'query.html', context)
+
+"""
+def giornalisti_list_api(request):
+    giornalisti=Giornalista.objects.all()
+    data={'giornalisti':list(giornalisti.values("pk","nome","cognome"))}
+    response=JsonResponse(data)
+    return response
+
+def giornalista_api(request,pk):
+    try:
+        giornalista=Giornalista.objects.get(pk=pk)
+        data={'giornalista':{
+            "nome":giornalista.nome,
+            "cognome":giornalista.cognome,
+            }
+        }
+        response=JsonResponse(data)
+    except Giornalista.DoesNotExist:
+        response=JsonResponse({
+            "error":{
+                "code":404,
+                "message":"Giornalista non trovato!"
+            }},
+            status=404)
+    return response
+
+
+def articoli_list_api(request):
+    articoli=Articolo.objects.all()
+    data={'articoli':list(articoli.values("pk","titolo","contenuto","giornalista","data","visualizzazioni"))}
+    response=JsonResponse(data)
+    return response
+
+def articolo_api(request,pk):
+    try:
+        articolo=Articolo.objects.get(pk=pk)
+        data={'articolo':{
+            "titolo":articolo.titolo,
+            "contenuto":articolo.contenuto,
+            "giornalista":articolo.giornalista,
+            "data":articolo.data,
+            "visualizzazioni":articolo.visualizzazioni,
+            }
+        }
+        response=JsonResponse(data)
+    except Articolo.DoesNotExist:
+        response=JsonResponse({
+            "error":{
+                "code":404,
+                "message":"Articolo non trovato!"
+            }},
+            status=404)
+    return response
+"""

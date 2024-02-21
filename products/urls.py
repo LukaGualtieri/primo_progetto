29162ac9d6b7c53fff2aa951f0b9f08d6edd6145
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2024-02-21 20:22:44 +0100

    aggiunta menu a tendina

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from products.views import (product_detail, product_list,
                             manufacturer_detail, manufacturer_list)
 
 app_name = 'products'
+
 urlpatterns = [
     path('products/', product_list, name='product-list'),
     path('products/<int:pk>/', product_detail, name="product-detail"),
